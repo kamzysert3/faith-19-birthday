99363c4257d8f6eb0f0769cb94a136c9ecec99e4
kamzysert3 <111480156+kamzysert3@users.noreply.github.com>
2025-01-05 16:34:45 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
      <Head>
-        <title>Our Love Story</title>
+        <title>Happy Birthday Love</title>
         <link rel="icon" type="image/png" href="/images/gift.png" />
         <meta name="description" content="A special journey for us" />
       </Head>
